1acbf37f6079a5218762e38d4bc0a1f68095ca2b
Raj-ab2 <rmuthazhagu@ab2consulting.com>
2022-01-03 20:56:46 +0530

    check positive flow

--- src/test/java/com/mousiki/testbase/BrowserFactory.java
+++ src/test/java/com/mousiki/testbase/BrowserFactory.java
@@ -41,8 +41,8 @@ public class BrowserFactory {
 	        	ChromeOptions options = new ChromeOptions();
 	        	if(System.getProperty("user.dir").indexOf("jenkins") > -1) {
 	        		// chrome binary location specified here
-	        		options.setBinary("/usr/bin/google-chrome");
-//		        	options.addArguments("--headless");
+//	        		options.setBinary("/usr/bin/google-chrome");
+		        	options.addArguments("--headless");
 		        	options.addArguments("start-maximized"); // open Browser in maximized mode
 		        	options.addArguments("disable-infobars"); // disabling infobars
 		        	options.addArguments("--disable-extensions"); // disabling extensions
