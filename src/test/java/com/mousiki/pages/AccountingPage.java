1e8a02410c09987039cbff547ab642a73ea47867
veenatl <veenapackiadhas2@gmail.com>
2022-02-25 17:24:17 +0700

    veena codes

---
+++ src/test/java/com/mousiki/pages/AccountingPage.java
@@ -0,0 +1,131 @@
+package com.mousiki.pages;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import com.mousiki.testbase.TestBase;
+
+public class AccountingPage extends TestBase {
+
+	WebDriver driver;
+	DashboardPage dashboardPg;
+	HomePage homePg;
+	// List of all WebElements in Accounting Page
+	public By studentDD_Xpath = By.xpath("//div[contains(@class,'css-1hwfws3')]/div[contains(.,'Student Name')]");
+	public By availableCourseCheckbx_Xpath = By.xpath("//label[contains(@class,'form-check-label')]");
+	public By invSave_Xpath = By.xpath("//button[contains(.,'Save')and contains(@class,'btn  ml-0 btn-primary')]");
+	public By invCreatedMsg = By.xpath("//h2[contains(@class,'page-heading-title new-invoice')]");
+	public By reportsLabel_Xpath = By.xpath("//span[contains(.,'Reports')]");
+	public By invNoInReports_Xpath = By.xpath(
+			"//div[contains(@class,'table-format__cell table-format__invoice_no table-format__invoice_no_data tbl-invoice-number')]");
+	public By ddValue_Xpath = By
+			.xpath("//div[@class=' css-1uccc91-singleValue']//div[contains(text(),'TestStudent QA')]");
+	// ("//div[contains(@class,'multiselect-option')]/div[contains(.,'TestStudent
+	// QA')]");
+	public By navBar_Xpath = By.xpath("//i[@class='la la-bars']");
+	public By accounting_Xpath = By
+			.xpath("//a[@class='collapsible-header Ripple-parent arrow-r active las leftnav__accounting']");
+	public By reports_Xpath = By.xpath("//span[normalize-space()='Reports']");
+	public By reportHeaderList_Xpath = By.xpath("//table[contains(@class,'template_tbl')]/thead/tr/td");
+
+	// List of all Strings to compare or enter in text field
+	public String strStudentName = "TestStudent QA";
+	public String strCourseCheckbx = "Course Checkbox";
+	public String strSaveBtn = "Save Button";
+	public String strInvCreationMsg = "Edit Invoice Message";
+	public String strInvNoInReport = "Invoice No in Report";
+	public String strReportsLabel = "Accounting-Reports";
+	public String strNavBar = "Navigation bar";
+	public String strAccountingLabel = "Accounting";
+	public String[] expectedListOfColHeaders = { "Description", "Qty", "Rate", "Discount", "Amount" };
+
+	// List of webelement Strings
+	public String strStudentDDTxt = "Student Name";
+
+	public AccountingPage(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	public void createInvoice() throws IOException, InterruptedException {
+		//click(driver, studentDD_Xpath, strStudentDDTxt);
+		driver.findElement(By.xpath("//div[contains(@class,' css-1wa3eu0-placeholder') and contains(.,'Student Name') ]")).click();
+		Thread.sleep(10000);
+		reportlog("Clicked DD", "PASS", strStudentDDTxt);
+		if (checkelementexists(driver, 0, studentDD_Xpath)) {
+			driver.findElement(By.xpath("//div[contains(@class,'multiselect-option')]/div[contains(.,'TestStudent QA')]")).click();
+			// reportlog("Selected student", "PASS", strStudentDDTxt);
+			/*
+			 * driver.findElement(ddValue_Xpath).sendKeys(Keys.ARROW_DOWN);
+			 * driver.findElement(ddValue_Xpath).sendKeys(Keys.ENTER);
+			 */
+			reportlog("Selected student", "PASS", strStudentDDTxt);
+			waitForLoad(driver);
+			click(driver, availableCourseCheckbx_Xpath, strCourseCheckbx);
+
+			reportlog("Selected course", "PASS", strCourseCheckbx);
+			click(driver, invSave_Xpath, strSaveBtn);
+
+			reportlog("Clicked Save", "PASS", strSaveBtn);
+		} else {
+			reportlog("Invoice failed to create", "FAIL", "Invoice failed to create");
+		}
+	}
+
+	public void validateInvID() throws IOException, InterruptedException {
+		String stractualText = getelementtext(driver, invCreatedMsg, strInvCreationMsg);
+		String trimedText = stractualText.replace("Edit Invoice ", "");
+		System.out.println(trimedText + "is the actual text");
+		Thread.sleep(3000);
+		click(driver, navBar_Xpath, strNavBar);
+		Thread.sleep(3000);
+		if (driver.findElement(accounting_Xpath).isEnabled()) {
+			click(driver, accounting_Xpath, strNavBar);
+			Thread.sleep(5000);
+			System.out.println("Accounting clicked successfully" + accounting_Xpath);
+
+			if (driver.findElement(reports_Xpath).isEnabled()) {
+				click(driver, reports_Xpath, strReportsLabel);
+				reportlog("Clicked reports", "PASS", strSaveBtn);
+				waitForLoad(driver);
+			}
+		} else {
+			System.out.println("Accounting  and reports failed to click");
+		}
+
+		String strActualInvNo = getelementtext(driver, invNoInReports_Xpath, strInvNoInReport);
+		if (strActualInvNo.equalsIgnoreCase(trimedText)) {
+			System.out.println("ACtual inv.no is:" + strActualInvNo);
+		} else {
+			System.out.println("Invoice no is not matching");
+		}
+
+	}
+
+	public void clickOnInvNum() throws IOException {
+		click(driver, invNoInReports_Xpath, strInvNoInReport);
+	}
+
+	public void validateInvoiceViewPage() throws InterruptedException {
+		Thread.sleep(10000);
+		List<WebElement> reportColHeaderList = driver.findElements(reportHeaderList_Xpath);
+		System.out.println(driver.findElement(reportHeaderList_Xpath).getAttribute("innerHTML"));
+		System.out.println("Actual list is:" + reportColHeaderList.size());
+		for (int k = 1; k <= reportColHeaderList.size(); k++) {
+			System.out.println("Loop:" + k);
+			By reportHeader_Xpath = By.xpath("//table[contains(@class,'template_tbl')]/thead/tr/td[" + k + "]");
+			System.out.println(driver.findElement(reportHeader_Xpath).getAttribute("innerHTML"));
+			if ((driver.findElement(reportHeader_Xpath).getAttribute("innerHTML"))
+					.equalsIgnoreCase(expectedListOfColHeaders[k - 1])) {
+				System.out.println("Both are matching");
+			} else
+				System.out.println("Did not match");
+		}
+
+	}
+}
---
+++ src/test/java/com/mousiki/pages/DashboardPage.java
@@ -0,0 +1,118 @@
+package com.mousiki.pages;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+
+import com.mousiki.testbase.TestBase;
+
+public class DashboardPage extends TestBase {
+
+	WebDriver driver;
+	public static String parent_common_xpath = "//h5[contains(.,'%s')]";
+	public static String tch_invoiceCommon_Xpath = "//p[contains(.,'%s')]";
+	public static String tch_attendancecomm_Xpath = "//p[contains(.,'%s')]//following-sibling::div[@class=' css-2b097c-container']";
+	public static String tch_Attendance_FromDateXpath = "//input[@name='fromDate' and @placeholder='D/M/YYYY']";
+	public static String tch_Attendance_ToDateXpath = "//input[@name='toDate' and @placeholder='D/M/YYYY']";
+	public By tch_UC_FullInfo_Xpath = By.xpath(
+			"//div[@class='classes-list__right-cont--info-wrapper'] | //div[@class='classes-list__left-cont--info-wrapper']");
+	public By tch_OpenInvoiceLink_Xpath = By.xpath("//p[contains(.,'Open Invoice(s)')]");
+	public static String upcmngSecTxt = "Upcoming Classes";
+	public static String invoicetxt = "Invoice Summary";
+	public static String classTrendstxt = "Class Trends (3 Months)";
+	public static String notificationtxt = "Notifications";
+	public static String parentPerftxt = "Your Performance";
+	public static String[] tch_UC_BubbleText = { "TestStudent QA", "Level-1 Coaching", "TQ" };
+	public static String tch_FeesCollecTrend_txt = "Fees Collection Trend (3 Months)";
+	public static String tch_CourseTrend_txt = "Course Trend (3 months)";
+	public static String tch_Attendance_txt = "Attendance";
+	public static String tch_Student_attdnc_txt = "Student";
+	public static String tch_Enrollment_txt = "Enrollment";
+	public static String tch_OpenInvoice_txt = "Open Invoice(s)";
+	public static String tch_OverdueInvoice_txt = "Overdue Invoice(s)";
+	public static String invoiceExpURL = "https://qa.mousiki.io/accounting/reports";
+	public  By accounting_Xpath=By.xpath("//a[@class='collapsible-header Ripple-parent arrow-r las leftnav__accounting']");
+	public By createInv_Xpath=By.xpath("//span[normalize-space()='Create Invoice']");
+	
+	//List of WebElements name
+	public String strAccountingTxt="Accounting Label";
+	public String strCreateInvTxt="Create Invoice Label";
+
+	public By invoiceColHeader_XPATH = By.xpath("//form[@name='contactForm']/./div[@class='sales-formtable-header'])");
+
+	public DashboardPage(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	public void validateColHeaders() {
+
+		List<WebElement> eleColHeadList = driver.findElements(invoiceColHeader_XPATH);
+		waitForLoad(driver);
+		String strColHeader = eleColHeadList.get(eleColHeadList.size() - 1).getText();
+		System.out.println("Column headers are:" + strColHeader);
+	}
+
+	public boolean dashboardValidation(String commonXpath, String sectionType) {
+		boolean bVal = false;
+		String replacedxpath = commonXpath.replace("%s", sectionType);
+		WebElement replacedXpath = driver.findElement(By.xpath(replacedxpath));
+		if (replacedXpath.isEnabled()) {
+			String actualtxt = driver.findElement(By.xpath(replacedxpath)).getText();
+			if (actualtxt.equalsIgnoreCase(sectionType)) {
+				System.out.println("Verified " + actualtxt + "section successfully");
+				bVal = true;
+			}
+		}
+		return bVal;
+	}
+
+	public boolean scrollAndValidate(String commonXpath, String sectionType) {
+		boolean bvalue = false;
+		String replacedxpath = commonXpath.replace("%s", sectionType);
+		WebElement replacedXpath = driver.findElement(By.xpath(replacedxpath));
+		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", replacedXpath);
+		System.out.println("Scrolled to:" + replacedXpath);
+		waitForLoad(driver);
+
+		if (replacedXpath.isDisplayed()) {
+			System.out.println("Verified " + sectionType + "section successfully");
+			bvalue = true;
+		} else
+			System.out.println("Unable to validate" + sectionType + "section");
+		return bvalue;
+
+	}
+
+	public void upcomingClassesInfoValidation() {
+		List<WebElement> allText = driver.findElements(tch_UC_FullInfo_Xpath);
+		String actualBubbleTexts = allText.get(allText.size() - 1).getText();
+		System.out.println(actualBubbleTexts);
+	}
+
+	public void clickOnInvoice() throws IOException {
+		click(driver, tch_OpenInvoiceLink_Xpath, tch_OpenInvoice_txt);
+		waitForLoad(driver);
+	}
+
+	public void checkNavigatedURL() throws IOException {
+		String actualURL = driver.getCurrentUrl();
+		if (actualURL.equalsIgnoreCase(invoiceExpURL)) {
+			reportlog("Navigated to Invoice report page", "PASS", "Navigated to Invoice page");
+		} else
+			reportlog("Unable to navigate to Invoice Report page", "FAIL", "Failed to navigate to Invoice Report page");
+	}
+
+	public void navigateToCreateInvoice() throws IOException {
+		click(driver, accounting_Xpath, strAccountingTxt);
+		waitForLoad(driver);
+		click(driver, createInv_Xpath, strCreateInvTxt);
+		waitForLoad(driver);
+		
+	}
+}
